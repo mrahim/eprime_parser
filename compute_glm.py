21591a2a022e156a2e2608d26f27fc6cc5bbdc4a
Mehdi Rahim <mehdi.rahim@cea.fr>
2014-12-15 18:19:04 +0100

    computing all contrasts

--- compute_glm.py
+++ compute_glm.py
@@ -1,8 +1,10 @@
 """
-    Compute GLM for psydat subjects.
+    * Compute GLM for psydat subjects.
     Inputs are :
     - design matrix CSV 
     - fMRI filelist
+    
+    * Compute contrasts
 
 """
 import os, glob
@@ -50,9 +52,60 @@ n_columns = len(design_mat.names)
 for i in range(14):
     contrasts['%s' % design_mat.names[2 * i]] = np.eye(n_columns)[2 * i]
 
-
-# TODO : add all contrasts
+# TODO : specify and estimate all contrasts
 contrasts['press R-L'] = contrasts['press_right'] - contrasts['press_left']
+
+contrast_list = ['anticip',
+                 'anticip_hit',
+                 'anticip_missed',
+                 'anticip_noresp',
+                 'anticip_hit-missed',
+                 'anticip_missed-hit',
+                 'anticip_hit-noresp',
+                 'anticip_noresp-hit',
+                 'anticip_hit_largewin-smallwin',
+                 'anticip_hit_largewin-nowin',
+                 'anticip_hit_smallwin-nowin',
+                 'anticip_missed_largewin-smallwin',
+                 'anticip_missed_largewin-nowin',
+                 'anticip_missed_smallwin-nowin',
+                 'anticip-anticip_noresp',
+                 'feedback',
+                 'feedback_hit',
+                 'feedback_missed',
+                 'feedback_hit-missed',
+                 'feedback_missed-hit',
+                 'feedback_hit_largewin-smallwin',
+                 'feedback_hit_largewin-nowin',
+                 'feedback_hit_smallwin-nowin',
+                 'feedback_missed_largewin-smallwin',
+                 'feedback_missed_largewin-nowin',
+                 'feedback_missed_smallwin-nowin',
+                 'press L+R',
+                 'press L-R',
+                 'press R-L',
+                 'anticip_hit_somewin-nowin',
+                 'anticip_missed_somewin-nowin',
+                 'feedback_hit_somewin-nowin',
+                 'feedback_missed_somewin-nowin',
+                 'feedback_somewin_hit-missed',
+                 'feedback_somewin_missed-hit',
+                 'feedback_somewin-nowin',
+                 'anticip_hit_largewin',
+                 '-anticip_hit_largewin',
+                 'feedback_hit_largewin',
+                 '-feedback_hit_largewin',
+                 'anticip_hit_largewin-feedback_hit_largewin',
+                 'feedback_hit_largewin-anticip_hit_largewin',
+                 'anticip_hit_nowin-feedback_hit_nowin',
+                 'feedback_hit_nowin-anticip_hit_nowin',
+                 'anticip_largewin-smallwin (hit+missed)',
+                 'anticip_largewin-nowin (hit+missed)',
+                 'anticip_smallwin-nowin (hit+missed)',
+                 'feedback_largewin-smallwin (hit+missed)',
+                 'feedback_largewin-nowin (hit+missed)',
+                 'feedback_smallwin-nowin (hit+missed)']
+
 
 contrast_id = 'press R-L'
 z_map, t_map, eff_map, var_map = glm.contrast(contrasts[contrast_id],
@@ -61,3 +114,7 @@ z_map, t_map, eff_map, var_map = glm.contrast(contrasts[contrast_id],
                                               output_stat=True,
                                               output_effects=True,
                                               output_variance=True)
+
+# TODO : save maps
+# TODO : save reports
+# 

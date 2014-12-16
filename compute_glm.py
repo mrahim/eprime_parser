f336ed16ca59130a82019c4d78abcf3b087540c7
Mehdi Rahim <mehdi.rahim@cea.fr>
2014-12-16 18:53:46 +0100

    glm, contrasts, and reports

--- compute_glm.py
+++ compute_glm.py
@@ -8,125 +8,138 @@
 
 """
 import os, glob
+from collections import OrderedDict
 import numpy as np
+import pandas as pd
 import nibabel as nib
 from nipy.modalities.fmri import design_matrix
 from nipy.modalities.fmri.glm import FMRILinearModel
+from nilearn.plotting import plot_stat_map
+import pypreprocess.reporting.glm_reporter as glm_reporter
+import pypreprocess.reporting.base_reporter as base_reporter
 
 DM_DIR = os.path.join('design_matrices')
 FMRI_DIR = os.path.join('/', 'disk4t', 'mehdi', 'data',
                         'psydat', 'preprocess_output')
 
-subj_id = '15143'
 
-# load design matrix
-dm_file = os.path.join(DM_DIR, subj_id+'.csv')
-design_mat = design_matrix.dmtx_from_csv(dm_file)
+subject_path_list = sorted(glob.glob(os.path.join(FMRI_DIR, 'S*')))
 
-# load anat
-anat_file = glob.glob(os.path.join(FMRI_DIR, 'S' + subj_id,
-                                   'anat',
-                                   'w*.img'))[0]
-
-# create 4D fmri
-fmri_files = sorted(glob.glob(os.path.join(FMRI_DIR,
-                                           'S' + subj_id,
-                                           'func',
-                                           'wr*.nii')))
-img4d = []
-img_data = []
-for fmri_file in fmri_files:
-    img = nib.load(fmri_file)
-    img_data.append(img.get_data())
-img_data = np.array(img_data)
-img_data = img_data.transpose([1,2,3,0])
-img4d = nib.Nifti1Image(img_data, img.get_affine())
-
-# computer glm
-glm = FMRILinearModel(img4d, design_mat.matrix, mask='compute')
-glm.fit(do_scaling=True, model='ar1')
-
-# specify contrasts
-contrasts = {}
-n_columns = len(design_mat.names)
-for i in range(14):
-    contrasts['%s' % design_mat.names[2 * i]] = np.eye(n_columns)[2 * i]
-
-# TODO : specify and estimate all contrasts
-# 1- load contrasts into pandas dataframe
-# 2- insert zero-columns of derivatives and regressors
-# 3- extract matrix :
-"""
-a = []
-for c in contrasts.keys():
-    if c != 'contrast':
-        a.append(contrasts[c].values)
-a = np.array(a)
-"""
-
-
-contrasts['press R-L'] = contrasts['press_right'] - contrasts['press_left']
-
-contrast_list = ['anticip',
-                 'anticip_hit',
-                 'anticip_missed',
-                 'anticip_noresp',
-                 'anticip_hit-missed',
-                 'anticip_missed-hit',
-                 'anticip_hit-noresp',
-                 'anticip_noresp-hit',
-                 'anticip_hit_largewin-smallwin',
-                 'anticip_hit_largewin-nowin',
-                 'anticip_hit_smallwin-nowin',
-                 'anticip_missed_largewin-smallwin',
-                 'anticip_missed_largewin-nowin',
-                 'anticip_missed_smallwin-nowin',
-                 'anticip-anticip_noresp',
-                 'feedback',
-                 'feedback_hit',
-                 'feedback_missed',
-                 'feedback_hit-missed',
-                 'feedback_missed-hit',
-                 'feedback_hit_largewin-smallwin',
-                 'feedback_hit_largewin-nowin',
-                 'feedback_hit_smallwin-nowin',
-                 'feedback_missed_largewin-smallwin',
-                 'feedback_missed_largewin-nowin',
-                 'feedback_missed_smallwin-nowin',
-                 'press L+R',
-                 'press L-R',
-                 'press R-L',
-                 'anticip_hit_somewin-nowin',
-                 'anticip_missed_somewin-nowin',
-                 'feedback_hit_somewin-nowin',
-                 'feedback_missed_somewin-nowin',
-                 'feedback_somewin_hit-missed',
-                 'feedback_somewin_missed-hit',
-                 'feedback_somewin-nowin',
-                 'anticip_hit_largewin',
-                 '-anticip_hit_largewin',
-                 'feedback_hit_largewin',
-                 '-feedback_hit_largewin',
-                 'anticip_hit_largewin-feedback_hit_largewin',
-                 'feedback_hit_largewin-anticip_hit_largewin',
-                 'anticip_hit_nowin-feedback_hit_nowin',
-                 'feedback_hit_nowin-anticip_hit_nowin',
-                 'anticip_largewin-smallwin (hit+missed)',
-                 'anticip_largewin-nowin (hit+missed)',
-                 'anticip_smallwin-nowin (hit+missed)',
-                 'feedback_largewin-smallwin (hit+missed)',
-                 'feedback_largewin-nowin (hit+missed)',
-                 'feedback_smallwin-nowin (hit+missed)']
-
-
-contrast_id = 'press R-L'
-z_map, t_map, eff_map, var_map = glm.contrast(contrasts[contrast_id],
-                                              con_id=contrast_id,
-                                              output_z=True,
-                                              output_stat=True,
-                                              output_effects=True,
-                                              output_variance=True)
-
-# TODO : save maps
-# TODO : save reports
-# 
+for subject_path in subject_path_list:
+    
+    _, subj_id = os.path.split(subject_path)
+    subj_id = subj_id[1:]
+    print subj_id
+    
+    # load design matrix
+    dm_file = os.path.join(DM_DIR, subj_id+'.csv')
+    design_mat = design_matrix.dmtx_from_csv(dm_file)
+    
+    # load anat
+    anat_file = glob.glob(os.path.join(FMRI_DIR, 'S' + subj_id,
+                                       'anat',
+                                       'w*.img'))[0]
+    anat_img = nib.load(anat_file)
+    anat = anat_img.get_data()
+    anat_affine = anat_img.get_affine()
+    
+    
+    # create 4D fmri
+    fmri_files = sorted(glob.glob(os.path.join(FMRI_DIR,
+                                               'S' + subj_id,
+                                               'func',
+                                               'wr*.nii')))
+    img4d = []
+    img_data = []
+    for fmri_file in fmri_files:
+        img = nib.load(fmri_file)
+        img_data.append(img.get_data())
+    img_data = np.array(img_data)
+    img_data = img_data.transpose([1,2,3,0])
+    img4d = nib.Nifti1Image(img_data, img.get_affine())
+    
+    # computer glm
+    glm = FMRILinearModel(img4d, design_mat.matrix, mask='compute')
+    glm.fit(do_scaling=True, model='ar1')
+    
+    # specify contrasts
+    contrasts = {}
+    n_columns = len(design_mat.names)
+    
+    # Specify and estimate all contrasts
+    # 1- load contrasts into pandas dataframe
+    contrast_df = pd.read_csv('contrasts.csv')
+    
+    # 2- insert zero-columns of derivatives and regressors
+    # regressors
+    for i in np.arange(28, n_columns):
+        contrast_df.insert(len(contrast_df.keys()),
+                           design_mat.names[i],
+                           np.zeros(50))
+    # derivatives
+    for i in np.arange(15, 1, -1):
+        contrast_df.insert(i, design_mat.names[2*i-3],
+                           np.zeros(50))
+    
+    # 3- extract matrix
+    contrast_mat = []
+    for c in contrast_df.keys():
+        if c != 'contrast':
+            contrast_mat.append(contrast_df[c].values)
+    contrast_mat = np.array(contrast_mat).T
+    
+    # 4- construct contrast dict
+    contrasts = OrderedDict()
+    for i in range(len(contrast_mat)):
+        contrasts[contrast_df['contrast'][i]] = contrast_mat[i,:]
+    
+    stat_map = {}
+    z_maps = {}
+    # Compute all contrasts
+    for contrast_id in contrasts.keys():
+        z_map, t_map, eff_map, var_map = glm.contrast(contrasts[contrast_id],
+                                                      con_id=contrast_id,
+                                                      contrast_type='t',
+                                                      output_z=True,
+                                                      output_stat=True,
+                                                      output_effects=True,
+                                                      output_variance=True)
+        stat_map['z_map'] = z_map
+        stat_map['t_map'] = t_map
+        stat_map['eff_map'] = eff_map
+        stat_map['var_map'] = var_map
+        
+        #plot_stat_map(stat_map['z_map'], anat_file, title=contrast_id, threshold=3)
+        
+        c_id = contrast_df[contrast_df['contrast']==contrast_id].index[0]+1
+        for map_type in stat_map.keys():
+            map_dir = os.path.join(FMRI_DIR, 'S' + subj_id, 'maps')
+            if not os.path.isdir(map_dir):
+                os.mkdir(map_dir)
+            map_type_dir = os.path.join(map_dir, map_type)
+            if not os.path.isdir(map_type_dir):
+                os.mkdir(map_type_dir)
+            map_path = os.path.join(map_type_dir,
+                                    'c' + "%03d" % (c_id) + '_' + contrast_id +'.nii.gz')
+            
+            if map_type == 'z_map':
+                z_maps[contrast_id] = map_path
+            nib.save(stat_map[map_type], map_path)
+    
+    
+    # do stats report
+    stats_report_filename = os.path.join(subject_path, 'reports',
+                                         "report_stats.html")
+    glm_reporter.generate_subject_stats_report(
+        stats_report_filename,
+        contrasts,
+        z_maps,
+        glm.mask,
+        design_matrices=[design_mat],
+        subject_id='S'+subj_id,
+        anat=anat,
+        anat_affine=anat_affine,
+        slicer='z',
+        cut_coords=5,
+        cluster_th=10  # we're only interested in this 'large' clusters
+        )
